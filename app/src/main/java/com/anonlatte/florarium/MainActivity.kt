8748494593e17c8f1dabbf9aecdeecd3a4be8f07
German Proshunin <proshunin.german@yandex.ru>
2020-07-27 15:39:10 +0500

    feature add logging

--- app/src/main/java/com/anonlatte/florarium/MainActivity.kt
+++ app/src/main/java/com/anonlatte/florarium/MainActivity.kt
@@ -2,10 +2,14 @@ package com.anonlatte.florarium
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
