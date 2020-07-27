4e04f40d1d35047dad3a51a309b4e884e77f2354
German Proshunin <proshunin.german@yandex.ru>
2020-07-27 16:12:29 +0500

    feature add addition info for creation layout

--- app/src/main/java/com/anonlatte/florarium/ui/creation/CreationFragment.kt
+++ app/src/main/java/com/anonlatte/florarium/ui/creation/CreationFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.TooltipCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.anonlatte.florarium.R
 import com.anonlatte.florarium.databinding.FragmentPlantCreationBinding
 
 class CreationFragment : Fragment() {
@@ -18,6 +20,28 @@ class CreationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentPlantCreationBinding.inflate(inflater, container, false)
+
+        makeScheduleItemsClickable()
+
         return binding.root
+    }
+
+    private fun makeScheduleItemsClickable() {
+        TooltipCompat.setTooltipText(
+            binding.wateringListItem.scheduleItem,
+            getString(R.string.tooltip_watering)
+        )
+        TooltipCompat.setTooltipText(
+            binding.sprayingListItem.scheduleItem,
+            getString(R.string.tooltip_spraying)
+        )
+        TooltipCompat.setTooltipText(
+            binding.fertilizingListItem.scheduleItem,
+            getString(R.string.tooltip_fertilizing)
+        )
+        TooltipCompat.setTooltipText(
+            binding.rotatingListItem.scheduleItem,
+            getString(R.string.tooltip_rotating)
+        )
     }
 }
