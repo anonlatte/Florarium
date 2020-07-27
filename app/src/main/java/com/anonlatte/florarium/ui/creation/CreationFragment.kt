63490660040a07faabd5c7da03feb19a247d8600
German Proshunin <proshunin.german@yandex.ru>
2020-07-27 18:01:02 +0500

    feature add bottom sheet dialog

--- app/src/main/java/com/anonlatte/florarium/ui/creation/CreationFragment.kt
+++ app/src/main/java/com/anonlatte/florarium/ui/creation/CreationFragment.kt
@@ -1,5 +1,7 @@
 package com.anonlatte.florarium.ui.creation
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +10,10 @@ import androidx.appcompat.widget.TooltipCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.anonlatte.florarium.R
+import com.anonlatte.florarium.databinding.BottomSheetBinding
 import com.anonlatte.florarium.databinding.FragmentPlantCreationBinding
+import com.anonlatte.florarium.databinding.ListItemScheduleBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class CreationFragment : Fragment() {
     private val viewModel by viewModels<CreationViewModel>()
@@ -21,9 +26,36 @@ class CreationFragment : Fragment() {
     ): View? {
         binding = FragmentPlantCreationBinding.inflate(inflater, container, false)
 
+        bindScheduleItemClickListener(binding.wateringListItem)
+        bindScheduleItemClickListener(binding.sprayingListItem)
+        bindScheduleItemClickListener(binding.fertilizingListItem)
+        bindScheduleItemClickListener(binding.rotatingListItem)
+
         makeScheduleItemsClickable()
 
         return binding.root
+    }
+
+    @SuppressLint("InflateParams")
+    private fun onScheduleItemClickListener(
+        title: String?,
+        icon: Drawable?
+    ) = View.OnClickListener {
+
+        val dialog = BottomSheetDialog(requireContext())
+        val dialogBinding = BottomSheetBinding.inflate(LayoutInflater.from(requireContext()))
+        dialog.setContentView(dialogBinding.root)
+        dialog.show()
+
+        dialogBinding.title = title
+        dialogBinding.icon = icon
+    }
+
+    private fun bindScheduleItemClickListener(itemScheduleBinding: ListItemScheduleBinding) {
+        with(itemScheduleBinding) {
+            executePendingBindings()
+            scheduleItem.setOnClickListener(onScheduleItemClickListener(title, icon))
+        }
     }
 
     private fun makeScheduleItemsClickable() {
