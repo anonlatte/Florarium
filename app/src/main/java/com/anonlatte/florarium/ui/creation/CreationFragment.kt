cc6e328d8959dd70aa16af0b30cbc36cc53e0522
German Proshunin <proshunin.german@yandex.ru>
2020-07-28 12:53:27 +0500

    feature handle switch events and update sliders values to captions

--- app/src/main/java/com/anonlatte/florarium/ui/creation/CreationFragment.kt
+++ app/src/main/java/com/anonlatte/florarium/ui/creation/CreationFragment.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
 import androidx.appcompat.widget.TooltipCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -14,6 +16,7 @@ import com.anonlatte.florarium.databinding.BottomSheetBinding
 import com.anonlatte.florarium.databinding.FragmentPlantCreationBinding
 import com.anonlatte.florarium.databinding.ListItemScheduleBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.list_item_schedule.view.*
 
 class CreationFragment : Fragment() {
     private val viewModel by viewModels<CreationViewModel>()
@@ -38,23 +41,77 @@ class CreationFragment : Fragment() {
 
     @SuppressLint("InflateParams")
     private fun onScheduleItemClickListener(
+        scheduleItemView: View?,
         title: String?,
         icon: Drawable?
-    ) = View.OnClickListener {
+    ) {
 
         val dialog = BottomSheetDialog(requireContext())
         val dialogBinding = BottomSheetBinding.inflate(LayoutInflater.from(requireContext()))
         dialog.setContentView(dialogBinding.root)
-        dialog.show()
 
         dialogBinding.title = title
         dialogBinding.icon = icon
+
+        setDialogListeners(dialog, dialogBinding, scheduleItemView)
+
+        addSliderListeners(dialogBinding)
+
+        dialog.show()
+
+    }
+
+    private fun setDialogListeners(
+        dialog: BottomSheetDialog,
+        dialogBinding: BottomSheetBinding,
+        scheduleItemView: View?
+    ) {
+        dialogBinding.okButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.setOnCancelListener {
+            scheduleItemView?.itemSwitch!!.isChecked = false
+        }
+
+        dialogBinding.cancelButton.setOnClickListener {
+            dialog.cancel()
+        }
+    }
+
+    private fun addSliderListeners(dialogBinding: BottomSheetBinding) {
+
+        with(dialogBinding.defaultIntervalItem) {
+            daySlider.addOnChangeListener { _, value, _ ->
+                title = getString(R.string.title_interval_in_days, value.toInt())
+            }
+        }
+
+        with(dialogBinding.winterIntervalItem) {
+            daySlider.addOnChangeListener { _, value, _ ->
+                title = getString(R.string.title_interval_for_winter, value.toInt())
+            }
+        }
+
+        with(dialogBinding.lastCareItem) {
+            daySlider.addOnChangeListener { _, value, _ ->
+                title = getString(R.string.title_last_care, value.toInt())
+            }
+        }
     }
 
     private fun bindScheduleItemClickListener(itemScheduleBinding: ListItemScheduleBinding) {
         with(itemScheduleBinding) {
             executePendingBindings()
-            scheduleItem.setOnClickListener(onScheduleItemClickListener(title, icon))
+            scheduleItem.setOnClickListener { onScheduleItemClickListener(it, title, icon) }
+            scheduleItem.itemSwitch.setOnTouchListener { switchView, event ->
+                if (event.action == MotionEvent.ACTION_DOWN && !(switchView as Switch).isChecked) {
+                    onScheduleItemClickListener(switchView, title, icon)
+                    switchView.performClick()
+                }
+                false
+
+            }
         }
     }
 
