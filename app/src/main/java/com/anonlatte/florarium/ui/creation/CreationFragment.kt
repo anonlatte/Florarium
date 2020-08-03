7d1195eff6d0d8b8e2910b05dedc570b92a4b9d0
German Proshunin <proshunin.german@yandex.ru>
2020-08-03 21:57:17 +0500

    feature select or take image in creation fragment

--- app/src/main/java/com/anonlatte/florarium/ui/creation/CreationFragment.kt
+++ app/src/main/java/com/anonlatte/florarium/ui/creation/CreationFragment.kt
@@ -1,8 +1,14 @@
 package com.anonlatte.florarium.ui.creation
 
-import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
@@ -12,13 +18,21 @@ import androidx.appcompat.widget.TooltipCompat
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.anonlatte.florarium.R
 import com.anonlatte.florarium.databinding.BottomSheetBinding
 import com.anonlatte.florarium.databinding.FragmentPlantCreationBinding
 import com.anonlatte.florarium.databinding.ListItemScheduleBinding
 import com.anonlatte.florarium.db.models.ScheduleType
+import com.anonlatte.florarium.utilities.REQUEST_IMAGE_CAPTURE
+import com.anonlatte.florarium.utilities.REQUEST_IMAGE_SELECT
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.FileDescriptor
 
 class CreationFragment : Fragment() {
     private val viewModel by viewModels<CreationViewModel>()
@@ -33,13 +47,7 @@ class CreationFragment : Fragment() {
         _binding = FragmentPlantCreationBinding.inflate(inflater, container, false)
         _binding!!.viewModel = viewModel
 
-        subScribeUI()
-
-        bindScheduleItemClickListener(binding.wateringListItem)
-        bindScheduleItemClickListener(binding.sprayingListItem)
-        bindScheduleItemClickListener(binding.fertilizingListItem)
-        bindScheduleItemClickListener(binding.rotatingListItem)
-
+        setListeners()
         makeScheduleItemsClickable()
 
         return binding.root
@@ -50,8 +58,30 @@ class CreationFragment : Fragment() {
         _binding = null
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                REQUEST_IMAGE_SELECT -> {
+                    resultData?.data?.also { uri ->
+                        lifecycleScope.launch {
+                            val bitmap = getBitmapFromUri(uri)
+                            binding.plantImageView.setImageBitmap(bitmap)
+                        }
+                    }
+                }
+                REQUEST_IMAGE_CAPTURE -> {
+                    resultData?.let {
+                        val imageBitmap = it.extras?.get("data") as Bitmap
+                        binding.plantImageView.setImageBitmap(imageBitmap)
+                    }
+                }
+            }
+        }
+    }
+
     // TODO add extended validation
-    private fun subScribeUI() {
+    private fun setListeners() {
         binding.addPlantButton.setOnClickListener {
             if (binding.titleEditText.text.isNullOrEmpty()) {
                 binding.titleInputLayout.error = getString(R.string.error_empty_plant_name)
@@ -73,15 +103,63 @@ class CreationFragment : Fragment() {
                 binding.titleInputLayout.error = null
             }
         }
+        binding.loadImage.setOnClickListener {
+            showIntentChooseDialog()
+        }
+        setScheduleItemListener(binding.wateringListItem)
+        setScheduleItemListener(binding.sprayingListItem)
+        setScheduleItemListener(binding.fertilizingListItem)
+        setScheduleItemListener(binding.rotatingListItem)
     }
 
-    @SuppressLint("InflateParams")
+    private fun showIntentChooseDialog() {
+        val multiItems = arrayOf("Camera", "Gallery")
+        MaterialAlertDialogBuilder(requireContext()).apply {
+            setTitle(getString(R.string.title_intent_image_select))
+            setItems(multiItems) { dialog, which ->
+                if (which == 0) {
+                    openImageCaptureIntent()
+                } else {
+                    openImageSelectIntent()
+                }
+                dialog.dismiss()
+            }
+            show()
+        }
+    }
+
+    private fun openImageSelectIntent() {
+        val imageSelectIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        startActivityForResult(imageSelectIntent, REQUEST_IMAGE_SELECT)
+    }
+
+    private fun openImageCaptureIntent() {
+        val imageCaptureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(requireActivity().packageManager)
+        }
+        startActivityForResult(imageCaptureIntent, REQUEST_IMAGE_CAPTURE)
+    }
+
+    private suspend fun getBitmapFromUri(uri: Uri): Bitmap? = withContext(Dispatchers.IO) {
+        val parcelFileDescriptor: ParcelFileDescriptor? =
+            requireActivity().contentResolver.openFileDescriptor(uri, "r")
+        var image: Bitmap? = null
+        parcelFileDescriptor?.use {
+            val fileDescriptor: FileDescriptor = parcelFileDescriptor.fileDescriptor
+            image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+            parcelFileDescriptor.close()
+        }
+        image
+    }
+
     private fun onScheduleItemClickListener(
         itemScheduleBinding: ListItemScheduleBinding,
         title: String?,
         icon: Drawable?
     ) {
-
         val dialog = BottomSheetDialog(requireContext())
         val dialogBinding = BottomSheetBinding.inflate(LayoutInflater.from(requireContext()))
         dialog.setContentView(dialogBinding.root)
@@ -102,37 +180,30 @@ class CreationFragment : Fragment() {
         dialogBinding: BottomSheetBinding,
         itemScheduleBinding: ListItemScheduleBinding
     ) {
-
         dialogBinding.okButton.setOnClickListener {
             itemScheduleBinding.itemSwitch.isChecked = true
-
             updateCareSchedule(
                 dialogBinding,
                 itemScheduleBinding.scheduleItemType
             )
-
             dialog.dismiss()
         }
-
         dialogBinding.cancelButton.setOnClickListener {
             dialog.cancel()
         }
     }
 
     private fun addSliderListeners(dialogBinding: BottomSheetBinding) {
-
         with(dialogBinding.defaultIntervalItem) {
             daySlider.addOnChangeListener { _, value, _ ->
                 title = getString(R.string.title_interval_in_days, value.toInt())
             }
         }
-
         with(dialogBinding.winterIntervalItem) {
             daySlider.addOnChangeListener { _, value, _ ->
                 title = getString(R.string.title_interval_for_winter, value.toInt())
             }
         }
-
         with(dialogBinding.lastCareItem) {
             daySlider.addOnChangeListener { _, value, _ ->
                 title = getString(R.string.title_last_care, value.toInt())
@@ -140,7 +211,7 @@ class CreationFragment : Fragment() {
         }
     }
 
-    private fun bindScheduleItemClickListener(itemScheduleBinding: ListItemScheduleBinding) {
+    private fun setScheduleItemListener(itemScheduleBinding: ListItemScheduleBinding) {
         with(itemScheduleBinding) {
             executePendingBindings()
             scheduleItem.setOnClickListener {
@@ -204,7 +275,6 @@ class CreationFragment : Fragment() {
         dialogBinding: BottomSheetBinding,
         scheduleTypeValue: Int?
     ) {
-
         val defaultIntervalValue = dialogBinding.defaultIntervalItem.daySlider.value.toInt()
         val winterIntervalValue = dialogBinding.winterIntervalItem.daySlider.value.toInt()
         val lastCareValue = dialogBinding.lastCareItem.daySlider.value.toInt()
--- app/src/main/java/com/anonlatte/florarium/utilities/Constants.kt
+++ app/src/main/java/com/anonlatte/florarium/utilities/Constants.kt
@@ -1,3 +1,5 @@
 package com.anonlatte.florarium.utilities
 
 const val DATABASE_NAME = "florarium-db"
+const val REQUEST_IMAGE_SELECT = 1
+const val REQUEST_IMAGE_CAPTURE = 2
