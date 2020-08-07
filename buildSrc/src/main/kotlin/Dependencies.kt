5cedc2234a7baf6c3d0b83d92a43da80a3aea1e6
German Proshunin <proshunin.german@yandex.ru>
2020-08-07 17:04:39 +0500

    update library appcompat

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,7 +3,7 @@ private const val ktlintVersion = "9.3.0"
 private const val checkUpdatesPluginVersion = "1.0.209"
 
 // Compile dependencies
-private const val appCompatVersion = "1.1.0"
+private const val appCompatVersion = "1.2.0"
 private const val archCoreVersion = "2.1.0"
 private const val constraintLayoutVersion = "1.1.3"
 private const val coreVersion = "1.3.1"
