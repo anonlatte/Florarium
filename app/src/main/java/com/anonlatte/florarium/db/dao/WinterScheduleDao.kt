7e40e3da87dd73a4268ff3b7977184b5174f10ba
German Proshunin <proshunin.german@yandex.ru>
2020-08-23 23:43:17 +0500

    test add to all dao classes

--- app/src/main/java/com/anonlatte/florarium/db/dao/WinterScheduleDao.kt
+++ app/src/main/java/com/anonlatte/florarium/db/dao/WinterScheduleDao.kt
@@ -7,9 +7,6 @@ import com.anonlatte.florarium.db.models.WinterSchedule
 
 @Dao
 interface WinterScheduleDao : BaseDao<WinterSchedule> {
-    @Query("SELECT * FROM winter_schedule WHERE plantId=:plantId")
-    fun getSchedule(plantId: Long): LiveData<WinterSchedule>
-
     @Query("SELECT * FROM winter_schedule")
     fun getSchedules(): LiveData<List<WinterSchedule>>
 }
