43bc2e5a8ce1b43a543f0c6120a5ae6ed22f1b7c
Herman Proshunin <g.proshunin15@gmail.com>
2023-07-19 20:46:21 +0500

    Add bottom navigation

--- app/src/main/java/com/anonlatte/florarium/ui/MainActivity.kt
+++ app/src/main/java/com/anonlatte/florarium/ui/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.anonlatte.florarium.R
+import com.anonlatte.florarium.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,15 +17,44 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private val appBarConfiguration by lazy { AppBarConfiguration(navController.graph) }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val navHostFragment = supportFragmentManager.findFragmentById(
             R.id.nav_host_fragment
         ) as NavHostFragment
         navController = navHostFragment.navController
         setupActionBarWithNavController(navController, appBarConfiguration)
+        setupNavigationBar()
+    }
+
+    private fun setupNavigationBar() {
+        binding.bottomNavigation.selectedItemId = R.id.nav_home
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_plants -> {
+                    // navController.navigate(R.id.plantsFragment)
+                    false
+                }
+
+                R.id.nav_home -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+
+                R.id.nav_profile -> {
+                    // navController.navigate(R.id.profileFragment)
+                    false
+                }
+
+                else -> false
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
